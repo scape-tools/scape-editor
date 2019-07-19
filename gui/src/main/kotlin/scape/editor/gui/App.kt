fef404cf09a39ea50437e6d511f7483bcf21f9a9
chadalen <nshusa99@gmail.com>
2019-07-19 00:45:06 -0500

    Fix running program on Java 11-12

--- gui/src/main/kotlin/scape/editor/gui/App.kt
+++ gui/src/main/kotlin/scape/editor/gui/App.kt
@@ -39,7 +39,7 @@ class App : Application() {
     }
 
     companion object {
-        val VERSION = "3.0.0"
+        val VERSION = "3.0.1"
 
         val fs = RSFileSystem()
 
---
+++ gui/src/main/kotlin/scape/editor/gui/Launcher.kt
@@ -0,0 +1,10 @@
+package scape.editor.gui
+
+object Launcher {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        App.main(args)
+    }
+
+}
